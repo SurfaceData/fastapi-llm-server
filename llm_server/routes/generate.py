627de3a7814cefec8827e47684b9391bb6080685
Keith Stevens <fozziethebeat@gmail.com>
2023-02-12 08:29:44 +0000

    Chop off the prompt prefix when generating the output

--- llm_server/routes/generate.py
+++ llm_server/routes/generate.py
@@ -27,5 +27,6 @@ def generate(request: GenerateRequest) -> List[GenerateResult]:
         outputs = model.generate(
             **tokenized_input, num_beams=request.n, num_return_sequences=request.n
         )
-    results = tokenizer.batch_decode(outputs)
+    input_ids = tokenized_input["input_ids"]
+    results = tokenizer.batch_decode(outputs[:, input_ids.shape[1] :])
     return [GenerateResult(completion=result) for result in results]
