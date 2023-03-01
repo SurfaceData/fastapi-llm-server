e6e1185a9a0c7d792d8a6abc2c711ab6ab02389b
Keith Stevens <fozziethebeat@gmail.com>
2023-03-01 06:17:27 +0000

    Adding a batch generate method

--- llm_server/config.py
+++ llm_server/config.py
@@ -5,8 +5,7 @@ from typing import Optional
 
 class Settings(BaseSettings):
     AUTH_TOKEN: Optional[str] = None
-    # The compute device available.
-    DEVICE: str = "cuda" if cuda.is_available() else "cpu"
+    DEVICE: Optional[int] = 0 if cuda.is_available() else None
     USE_GPU: bool = cuda.is_available()
     MODEL: str = "EleutherAI/gpt-neo-2.7B"
     MODEL_TYPE: str = "text-generation"
